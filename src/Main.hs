eabba2a4ff000577d551ecc38ceaaf16e115c5cb
Agustinnoli <165932040+Agustinnoli@users.noreply.github.com>
2025-04-10 16:48:47 -0300

    mas tests

--- src/Main.hs
+++ src/Main.hs
@@ -14,6 +14,7 @@ allTests =
     [ "Ejercicio 2" ~: testsEj2,
       "Ejercicio 3" ~: testsEj3,
       "Ejercicio 4" ~: testsEj4,
+      "Ejercicio 5" ~: testsEj5,
       "Ejercicio 6" ~: testsEj6,
       "Ejercicio 7" ~: testsEj7,
       "Ejercicio 8" ~: testsEj8,
@@ -59,31 +60,59 @@ testsEj4 =
   test
     [ mostrar vacio ~?= "",
       mostrar linea ~?= "\n",
-      mostrar (indentar 2 (texto "a" <+> linea <+> texto "b")) ~?= "a\n  b"
+      mostrar (indentar 2 (texto "a" <+> linea <+> texto "b")) ~?= "a\n  b",
+      mostrar (texto "a" <+> linea <+> linea <+> texto "b") ~?= "a\n\nb",
+      mostrar (texto "a" <+> linea <+> indentar 3 (linea <+> texto "b")) ~?= "a\n\n   b",
+      mostrar (texto "Hola" <+> linea <+> texto "Mundo" <+> linea <+> (texto "Hello" <+> linea <+> indentar 4 (linea <+> texto "World" ))  ) ~?= "Hola\nMundo\nHello\n\n    World"
     ]
--- :{
+
 pericles, merlina, addams, familias :: PPON
 pericles = ObjetoPP [("nombre", TextoPP "Pericles"), ("edad", IntPP 30)]
 merlina = ObjetoPP [("nombre", TextoPP "Merlina"), ("edad", IntPP 24)]
 addams = ObjetoPP [("0", pericles), ("1", merlina)]
 familias = ObjetoPP [("Addams", addams)]
--- :}
+
 --ppons de test propios
 nivel1, nivel2, nivel3, nivel4 :: PPON
 nivel1 = ObjetoPP [("info", TextoPP "Nivel 1"), ("siguiente", nivel2)]
 nivel2 = ObjetoPP [("info", TextoPP "Nivel 2"), ("siguiente", nivel3)]
 nivel3 = ObjetoPP [("info", TextoPP "Nivel 3"), ("siguiente", nivel4)]
 nivel4 = ObjetoPP [("info", TextoPP "Nivel 4")]
+objetolistavacia =ObjetoPP []
 
 pponAnidado :: Int -> PPON
 pponAnidado 0 = TextoPP "Nivel 0 Fin"
 pponAnidado n = ObjetoPP [("Nivel " ++ show n, pponAnidado (n-1))]
 
+testsEj5 :: Test
+testsEj5 =
+  test
+    [ pponAtomico pericles ~?= False,
+      pponAtomico  merlina~?= False,
+      pponAtomico  addams~?= False,
+      pponAtomico  familias~?= False,
+      pponAtomico  nivel1~?= False,
+      pponAtomico  nivel2~?= False,
+      pponAtomico  nivel3~?= False,
+      pponAtomico  nivel4~?= False,
+      pponAtomico  objetolistavacia~?= False,
+      pponAtomico  (TextoPP "a") ~?= True,
+      pponAtomico  (IntPP 2) ~?= True
+    ]
+
 testsEj6 :: Test
 testsEj6 =
   test
     [ pponObjetoSimple pericles ~?= True,
-      pponObjetoSimple addams ~?= False
+      pponObjetoSimple addams ~?= False,
+      pponObjetoSimple familias ~?= False,
+      pponObjetoSimple nivel1 ~?= False,
+      pponObjetoSimple nivel2 ~?= False,
+      pponObjetoSimple nivel3 ~?= False,
+      pponObjetoSimple nivel4 ~?= False,
+      pponObjetoSimple objetolistavacia ~?= True,
+      pponObjetoSimple (TextoPP "a") ~?= False,
+      pponObjetoSimple (IntPP 2) ~?= False
     ]
 
 a, b, c, l:: Doc
@@ -110,7 +139,12 @@ testsEj7 =
 testsEj8 :: Test
 testsEj8 =
   test
-    [ mostrar (aplanar (a <+> linea <+> b <+> linea <+> c)) ~?= "a b c"
+    [ mostrar (aplanar (a <+> linea <+> b <+> linea <+> c)) ~?= "a b c",
+      mostrar (aplanar (a  <+> b  <+> c)) ~?= "abc",
+      mostrar(aplanar (a <+> linea <+> b <+> linea <+> linea <+> linea <+> c)) ~?= "a b c",
+      aplanar (vacio) ~?= vacio,
+      aplanar (a) ~?= a,
+      mostrar (aplanar (linea)) ~?= ""
     ]
 
 testsEj9 :: Test
